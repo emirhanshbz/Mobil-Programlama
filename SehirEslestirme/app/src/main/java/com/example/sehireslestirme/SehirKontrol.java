5931d6d47f214fe78fb672e6875479730f8caa02
emirhan <emir0sahbaz@gmail.com>
2025-03-10 12:03:12 +0300

    Şehir Eşleştirme

--- SehirEslestirme/app/src/main/java/com/example/sehireslestirme/SehirKontrol.java
+++ SehirEslestirme/app/src/main/java/com/example/sehireslestirme/SehirKontrol.java
@@ -19,21 +19,18 @@ public class SehirKontrol extends AppCompatActivity {
 
         TextView txtSonuc = findViewById(R.id.txtSonuc);
 
-        // MainActivity'den gelen verileri alıyoruz
         Intent intent = getIntent();
         String plaka = intent.getStringExtra("plaka");
         String sehir = intent.getStringExtra("sehir");
 
-        // MainActivity'den plakaSehirMap'i alıyoruz
         MainActivity mainActivity = new MainActivity();
         Map<String, String> plakaSehirMap = mainActivity.getPlakaSehirMap();
 
-        // Doğru plaka ve şehri kontrol et
         if (plakaSehirMap.get(plaka).equals(sehir)) {
             Toast.makeText(this, "Doğru!", Toast.LENGTH_SHORT).show();
         } else {
             String dogruSehir = plakaSehirMap.get(plaka);
-            Toast.makeText(this, "Yanlış! Doğru: " + plaka + " - " + dogruSehir, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Yanlış! Doğru Plaka: " + plaka + " - " + dogruSehir, Toast.LENGTH_LONG).show();
         }
     }
 }
