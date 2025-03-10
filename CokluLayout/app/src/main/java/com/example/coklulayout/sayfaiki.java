9c900a8f2f6e9bab869418559e49c70e4c2a83b3
emirhan <emir0sahbaz@gmail.com>
2025-03-10 10:28:26 +0300

    Layout Ornek

---
+++ CokluLayout/app/src/main/java/com/example/coklulayout/sayfaiki.java
@@ -0,0 +1,30 @@
+package com.example.coklulayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class sayfaiki extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.sayfaiki);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListeners() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), sayfaiki.class));
+            }
+        });
+    }
+}
